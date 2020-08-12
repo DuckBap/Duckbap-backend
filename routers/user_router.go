eeb8474cd468a3aa67539f4505f557a473982a59
dokang <dokang@dokangui-MacBookPro.local>
2020-08-12 17:04:36 +0900

    create branch

--- routers/user_router.go
+++ routers/user_router.go
@@ -1,15 +1,14 @@
 package routers
 
 import (
-	"github.com/DuckBap/Duckbap-backend/middlewares"
 	"github.com/DuckBap/Duckbap-backend/controllers"
+	"github.com/DuckBap/Duckbap-backend/middlewares"
 	"github.com/gin-gonic/gin"
-
 )
 
 func SetUserRouters(r *gin.RouterGroup) {
 	r.POST("/login", middlewares.Auth.LoginHandler)
 	r.GET("/me", middlewares.Auth.MiddlewareFunc(), controllers.GetMe)
-	//r.GET("/me", middlewares.Auth.MiddlewareFunc(), <HANDLER>)
+	//r.GET("/me", middlewares.Auth.MiddlewareFunc(), adf)
 }
 
