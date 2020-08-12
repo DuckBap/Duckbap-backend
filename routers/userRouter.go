3102655c90ec247ebb9aaaf4e87341df8e5f8a3c
42 <eun-park@42ui-MacBookPro.local>
2020-08-12 21:06:07 +0900

    change json response struct

--- routers/userRouter.go
+++ routers/userRouter.go
@@ -10,6 +10,5 @@ import (
 func SetUserRouters(r *gin.RouterGroup) {
 	r.POST("/login", middlewares.Auth.LoginHandler)
 	r.GET("/me", middlewares.Auth.MiddlewareFunc(), controllers.GetMe)
-	//r.GET("/me", middlewares.Auth.MiddlewareFunc(), <HANDLER>)
 }
 
