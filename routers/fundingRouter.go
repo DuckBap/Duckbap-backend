d1cdfd03f89b283c2d3ba2f37c827439e6305f65
sujung <tjdfkr2421@gmail.com>
2020-08-12 21:49:26 +0900

    add func branchFunding

--- routers/fundingRouter.go
+++ routers/fundingRouter.go
@@ -8,20 +8,29 @@ import (
 	"strconv"
 )
 
-func SetFundingRouter (router *gin.RouterGroup) {
-	router.GET("/main", isLogined)
-	router.GET("/banner", controllers.BannerSelect)
-	router.GET("/fund/:fund-id", controllers.GetFunding)
+func SetFundingRouter(router *gin.RouterGroup) {
+	router.GET("/:fund-id", brachFunding)
 	router.GET("", controllers.GetFundingList)
 }
 
-func isLogined (c *gin.Context) {
-	test,err :=middlewares.Auth.GetClaimsFromJWT(c)
+func isLogined(c *gin.Context) {
+	test, err := middlewares.Auth.GetClaimsFromJWT(c)
 	if err == nil {
 		stringId := fmt.Sprintf("%v", test["id"])
-		id,_ := strconv.Atoi(stringId)
+		id, _ := strconv.Atoi(stringId)
 		controllers.ListSelect(c, uint(id))
 	} else {
 		controllers.NotloginListSelect(c)
+	}
+}
+
+func brachFunding(c *gin.Context) {
+	param := c.Param("fund-id")
+	if param == "main" {
+		isLogined(c)
+	} else if param == "banner" {
+		controllers.BannerSelect(c)
+	} else {
+		controllers.GetFunding(c)
 	}
 }
