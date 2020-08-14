41cce2165ed6f409d5df179145f97fd5a5839f38
dokang <9from6@gmail.com>
2020-08-14 16:54:24 +0900

    modify swagger value

--- docs/docs.go
+++ docs/docs.go
@@ -24,6 +24,50 @@ var doc = `{
     "host": "{{.Host}}",
     "basePath": "{{.BasePath}}",
     "paths": {
+        "/accounts/sign-up": {
+            "post": {
+                "description": "\u003cbr\u003e유저의 정보를 context에 저장하여 미들웨어에게 전달합니다.\u003cbr\u003e\n빈 값이 요청되었을 경우 오류 발생위치와 오류 메시지를 반환합니다.\u003cbr\u003e\n잘못된 값이 들어왔을 경우 오류 발생위치와 오류 메시지를 반환합니다.\u003cbr\u003e\n이미 회원인 경우 오류를 발생시켜 오류 발생위치와 오류 메시지를 반환합니다.\u003cbr\u003e",
+                "consumes": [
+                    "application/json"
+                ],
+                "produces": [
+                    "application/json"
+                ],
+                "summary": "회원가입 요청이 들어왔을 때 동작하는 곳",
+                "responses": {
+                    "200": {
+                        "description": "token",
+                        "schema": {
+                            "type": ""
+                        }
+                    },
+                    "208": {
+                        "description": "이미 존재한 값이 들어올 때\", \"{\"err\": {\"errorPoint\": \"message\"}}",
+                        "schema": {
+                            "type": ""
+                        }
+                    },
+                    "400": {
+                        "description": "잘못된 값이 들어올 때\", \"{\"err\": {\"errorPoint\": \"message\"}}",
+                        "schema": {
+                            "type": ""
+                        }
+                    },
+                    "404": {
+                        "description": "해당 값을 통해서 회원 가입을 못할 때\", \"{\"err\": {\"errorPoint\": \"message\"}}",
+                        "schema": {
+                            "type": ""
+                        }
+                    },
+                    "424": {
+                        "description": "참조할 수 없는 값이 들어올 때\", \"{\"err\": {\"errorPoint\": \"message\"}}",
+                        "schema": {
+                            "type": ""
+                        }
+                    }
+                }
+            }
+        },
         "/artists/": {
             "get": {
                 "description": "\u003cbr\u003e아티스트 리스트를 반환합니다.\u003cbr\u003e\n쿼리스트링이 존재하지 않을 경우 모든 아티스트를 반환합니다.\u003cbr\u003e\n쿼리스트링이 존재하는 경우 쿼리스트링을 조건으로 필터링 된 아티스트를 반환합니다.\u003cbr\u003e\n쿼리스트링 종류\n1. /v1/artists?ent-id=()",
