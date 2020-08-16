9414890217a0fee15174047331b821f734ca8afd
DiceMono <qrdfg@naver.com>
2020-08-17 00:59:38 +0900

    fix model declaring models varchar

--- models/artist.go
+++ models/artist.go
@@ -5,8 +5,8 @@ import "gorm.io/gorm"
 type Artist struct {
 	gorm.Model
 	EntertainmentID uint `gorm:"not null" json:"entertainmentId"`
-	Name string `gorm:"varchar(60); not null" json:"name"`
-	ImgUrl string `gorm:"varchar(255); unique; not null" json:"imgUrl"`
+	Name string `gorm:"type:varchar(60); not null" json:"name"`
+	ImgUrl string `gorm:"type:varchar(512); unique; not null" json:"imgUrl"`
 	Users []User `gorm:"many2many:bookmarks"`
 	Entertainment Entertainment
 }
