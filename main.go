891969c52f01a3f5356edab9e2de87a5329c1636
ParkSanggi <roqkfwkehlwk@naver.com>
2020-08-26 12:41:00 +0900

    fix cors registration

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 func main() {
 	var err error
 	r := gin.Default()
+	r.Use(cors.Default())
 
 	//newLogger := logger.New(
 	//	log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
@@ -35,7 +36,6 @@ func main() {
 		&models.Artist{}, &models.Receipt{}, &models.Entertainment{})
 
 	rGroup := r.Group("/v1")
-	rGroup.Use(cors.Default())
 	routers.SignUpRouter(rGroup.Group("/accounts"))
 	routers.SetUserRouters(rGroup.Group("/users"))
 	routers.SetArtistRouter(rGroup.Group("/artists"))
